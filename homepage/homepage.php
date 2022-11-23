4b2b1e42f660d1e644495575fe38d8c9672c3acc
khasmirmb <xt202001178@wmsu.edu.ph>
2022-11-23 20:29:28 +0800

    Added Image Slider

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -33,10 +33,64 @@
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Trending</a></li>
-                <li><a href="#">Discussion</a></li>
+                <li><a href="#">Vote Now!</a></li>
                 <li><a href="#">About</a></li>
             </ul>
             <button type="button" id="login-show">Login</button>
+            <button type="button" id="#">SignUp</button>
+    </div>
+        <section class="section">
+            <div class="slider">
+                <div class="slide">
+                    <input type="radio" name="radio-btn" id="radio1">
+                    <input type="radio" name="radio-btn" id="radio2">
+                    <input type="radio" name="radio-btn" id="radio3">
+                    <input type="radio" name="radio-btn" id="radio4">
+
+                    <div class="st first">
+                            <img src="../images/img1.jpg" alt="img" width="850" height="400">
+                    </div>
+
+                    <div class="st">
+                        <img src="../images/img2.jpg" alt="img" width="850" height="400">
+                    </div>
+
+                    <div class="st">
+                        <img src="../images/img3.jpg" alt="img" width="850" height="400">
+                    </div>
+
+                    <div class="st">
+                        <img src="../images/img4.jpg" alt="img" width="850" height="400">
+                    </div>
+
+                    <div class="nav-auto">
+                        <div class="a-b1"></div>
+                        <div class="a-b2"></div>
+                        <div class="a-b3"></div>
+                        <div class="a-b4"></div>
+                    </div>
+                </div>
+                <div class="nav-m">
+                    <label for="radio1" class="m-btn"></label>
+                    <label for="radio2" class="m-btn"></label>
+                    <label for="radio3" class="m-btn"></label>
+                    <label for="radio4" class="m-btn"></label>
+                </div>
+            </div>
+        </section>
+
+        <script type="text/javascript">
+            var counter=1;
+            setInterval(function(){
+                document.getElementById('radio' + counter).checked=true;
+                counter++;
+                if(counter > 4){
+                    counter = 1;
+                }
+            },5000);
+        </script>
+    <div class="home-sidebar">
+        
     </div>
 
     <div id="login-modal">
@@ -73,7 +127,6 @@
             });
         });
     </script>
-
 <?php
     require_once '../includes/footer.php';
 ?>
--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=p, initial-scale=1.0">
-    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="../css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
+
     <title>AniZone</title>
+
 </head>
 <body>
