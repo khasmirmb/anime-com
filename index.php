921753949556268e6d2bee0a11fcb570f23592a9
khasmirmb <xt202001178@wmsu.edu.ph>
2022-11-26 00:20:20 +0800

    as

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     session_start();
 
     //check if user is login already otherwise send to login page
-    if (isset($_SESSION['user_type']) == 'admin'){
-        header('location: user/adminpage.php');
+    if (isset($_SESSION['user_type']) == 'user'){
+        header('location: user/userpage.php');
     }
-    else if (isset($_SESSION['user_type']) == 'user'){
-        header('location: user/login.php');
+    else if (isset($_SESSION['user_type']) == 'admin'){
+        header('location: user/adminpage.php');
     }
     else{
         header('location: homepage/homepage.php');
